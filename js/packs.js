e7b33870457373f71e1279300149467e9ca23c39
Funlet Owner <blookettokens64@gmail.com>
2026-04-16 13:15:03 -0500

    Create packs.js

---
+++ js/packs.js
@@ -0,0 +1,20 @@
+const packs = [
+  {
+    id: "spooky",
+    name: "🎃 Spooky Pack",
+    month: 9,
+    price: 100
+  },
+  {
+    id: "autumn",
+    name: "🍂 Autumn Pack",
+    month: 10,
+    price: 120
+  },
+  {
+    id: "blizzard",
+    name: "❄️ Blizzard Pack",
+    month: 11,
+    price: 150
+  }
+];
